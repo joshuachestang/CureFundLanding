fcc3b3878c0f9cb97a9e8aa47f5b93195c83d21e
Joshua Chestang <joshua.chestang@gmail.com>
2015-03-01 17:34:51 -0600

    removing extra images, cleaning up design.

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -69,6 +69,6 @@ class SubscribersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def subscriber_params
-      params.require(:subscriber).permit(:email)
+      params.require(:subscriber).permit(:email, :subscriber_type)
     end
 end
